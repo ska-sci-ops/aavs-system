fa1d9f60cd4fdd07775c890a6a6cfc94c1f37bee
amattana <a.mattana80@gmail.com>
2023-07-10 16:05:19 +0200

    testing AAVS3 PreADU

--- python/utilities/skalab/skalab_preadu.py
+++ python/utilities/skalab/skalab_preadu.py
@@ -472,7 +472,7 @@ class AAVS3OpticalRx(Rx):
 
     @staticmethod
     def op_set_attenuation(code, att):
-        return int(att * 2)
+        return int(att * 8)
 
     @staticmethod
     def op_get_attenuation(code):
