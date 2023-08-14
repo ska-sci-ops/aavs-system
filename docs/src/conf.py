300baad8e66d8b044f351915b5ff35c727def5e5
dmb74885 <john.holden@stfc.ac.uk>
2023-08-14 13:15:55 +0100

    mccs-1566: cheacked docs theme to ska_ser_sphinx_theme

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 #sys.path.insert(0, os.path.abspath('../..'))
-import sphinx_rtd_theme
+import ska_ser_sphinx_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -98,7 +98,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'ska_ser_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
